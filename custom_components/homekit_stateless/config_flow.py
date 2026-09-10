f62354b4d3a74d6b219c6a9f96005f0672d78c37
berggrein <32194000+berggrein@users.noreply.github.com>
2026-09-10 05:04:44 +0200

    Initial

---
+++ custom_components/homekit_stateless/config_flow.py
@@ -0,0 +1,11 @@
+from homeassistant import config_entries
+from .const import DOMAIN
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="HomeKit Stateless Button Hub", data={})
+
+        return self.async_show_form(step_id="user")
